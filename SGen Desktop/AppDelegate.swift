f4f8c6d3d36aae0a0b0d44ff00dcabc017623ba9
Conner Vieira <vieirast@icloud.com>
2019-08-24 21:43:06 -0400

    Initial Commit

---
+++ SGen Desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SGen Desktop
+//
+//  Created by Conner Vieira on 8/24/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SGen Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SGen Desktop
+//
+//  Created by Conner Vieira on 8/24/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
